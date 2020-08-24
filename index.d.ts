71760949e246dc871aae0757a3b7e24b1fb2b520
Tim Kendrick <timkendrick@gmail.com>
2020-08-24 22:36:00 +0100

    Add callback and event emitter type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-import {Stats} from 'fs';
-import {Stream} from 'stream';
+import { Stats } from 'fs';
+import { Stream } from 'stream';
 
 interface Options {
 	/**
@@ -35,21 +35,83 @@ interface Options {
 	 *
 	 * Defaults to true.
 	 */
-	results?: boolean,
+	results?: boolean;
 	/**
 	 * Maximum number of simultaneous copy operations.
 	 *
 	 * Defaults to 255.
 	 */
-	concurrency?: number,
+	concurrency?: number;
 	/**
 	 * Whether to log debug information.
 	 */
-	debug?: boolean,
+	debug?: boolean;
 }
 
-export default function copy(
-	source: string,
-	dest: string,
-	options?: Options,
-): Promise<void>;
+interface CopyFn {
+	(
+		source: string,
+		dest: string,
+		options?: Options,
+	): WithCopyEvents<Promise<Array<CopyOperation>>>;
+	(
+		source: string,
+		dest: string,
+		callback: (error: Error | null, results?: Array<CopyOperation>) => void,
+	): WithCopyEvents<{}>;
+	events: {
+		ERROR: CopyEventType.ERROR;
+		COMPLETE: CopyEventType.COMPLETE;
+		CREATE_DIRECTORY_START: CopyEventType.CREATE_DIRECTORY_START;
+		CREATE_DIRECTORY_ERROR: CopyEventType.CREATE_DIRECTORY_ERROR;
+		CREATE_DIRECTORY_COMPLETE: CopyEventType.CREATE_DIRECTORY_COMPLETE;
+		CREATE_SYMLINK_START: CopyEventType.CREATE_SYMLINK_START;
+		CREATE_SYMLINK_ERROR: CopyEventType.CREATE_SYMLINK_ERROR;
+		CREATE_SYMLINK_COMPLETE: CopyEventType.CREATE_SYMLINK_COMPLETE;
+		COPY_FILE_START: CopyEventType.COPY_FILE_START;
+		COPY_FILE_ERROR: CopyEventType.COPY_FILE_ERROR;
+		COPY_FILE_COMPLETE: CopyEventType.COPY_FILE_COMPLETE;
+	};
+}
+
+declare const copy: CopyFn;
+export default copy;
+
+export interface CopyErrorInfo {
+	src: string;
+	dest: string;
+}
+
+export interface CopyOperation {
+	src: string;
+	dest: string;
+	stats: Stats;
+}
+
+type WithCopyEvents<T> = T & {
+	on(event: CopyEventType.ERROR, callback: (error: Error, info: CopyErrorInfo) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COMPLETE, callback: (info: Array<CopyOperation>) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+}
+
+export enum CopyEventType {
+	ERROR = 'error',
+	COMPLETE = 'complete',
+	CREATE_DIRECTORY_START = 'createDirectoryStart',
+	CREATE_DIRECTORY_ERROR = 'createDirectoryError',
+	CREATE_DIRECTORY_COMPLETE = 'createDirectoryComplete',
+	CREATE_SYMLINK_START = 'createSymlinkStart',
+	CREATE_SYMLINK_ERROR = 'createSymlinkError',
+	CREATE_SYMLINK_COMPLETE = 'createSymlinkComplete',
+	COPY_FILE_START = 'copyFileStart',
+	COPY_FILE_ERROR = 'copyFileError',
+	COPY_FILE_COMPLETE = 'copyFileComplete',
+}
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,10 +1,39 @@
-import copy from '.';
-import {Stream} from 'stream';
-import {expectType, expectError} from 'tsd';
+import copy, { CopyErrorInfo, CopyEventType, CopyOperation } from '.';
+import { Stream } from 'stream';
+import { expectError, expectType } from 'tsd';
 
+// Promise interface
 copy('source', 'dest')
+	.on(copy.events.ERROR, (error, info) => {})
+	.on(copy.events.COMPLETE, (info) => {})
+	.on(copy.events.CREATE_DIRECTORY_START, (info) => {})
+	.on(copy.events.CREATE_DIRECTORY_ERROR, (error, info) => {})
+	.on(copy.events.CREATE_DIRECTORY_COMPLETE, (info) => {})
+	.on(copy.events.CREATE_SYMLINK_START, (info) => {})
+	.on(copy.events.CREATE_SYMLINK_ERROR, (error, info) => {})
+	.on(copy.events.CREATE_SYMLINK_COMPLETE, (info) => {})
+	.on(copy.events.COPY_FILE_START, (info) => {})
+	.on(copy.events.COPY_FILE_ERROR, (error, info) => {})
+	.on(copy.events.COPY_FILE_COMPLETE, (info) => {})
 	.then(() => {})
 	.catch(e => {});
+
+// Callback interface
+copy('source', 'dest', (error, results) => {})
+	.on(copy.events.ERROR, (error, info) => {})
+	.on(copy.events.COMPLETE, (info) => {})
+	.on(copy.events.CREATE_DIRECTORY_START, (info) => {})
+	.on(copy.events.CREATE_DIRECTORY_ERROR, (error, info) => {})
+	.on(copy.events.CREATE_DIRECTORY_COMPLETE, (info) => {})
+	.on(copy.events.CREATE_SYMLINK_START, (info) => {})
+	.on(copy.events.CREATE_SYMLINK_ERROR, (error, info) => {})
+	.on(copy.events.CREATE_SYMLINK_COMPLETE, (info) => {})
+	.on(copy.events.COPY_FILE_START, (info) => {})
+	.on(copy.events.COPY_FILE_ERROR, (error, info) => {})
+	.on(copy.events.COPY_FILE_COMPLETE, (info) => {});
+
+// Prevent specifying both callback and promise interfaces
+expectError(copy('source', 'dest', (error, results) => {}).then(() => {}));
 
 // All options should be optional.
 copy('source', 'dest', {})
@@ -37,7 +66,24 @@ copy('source', 'dest', {filter: /abc/});
 copy('source', 'dest', {filter: ['abc', 'def']});
 copy('source', 'dest', {filter: (path) => false});
 
-expectType<Promise<void>>(copy('source', 'dest'));
+expectType<WithCopyEvents<Promise<Array<CopyOperation>>>>(copy('source', 'dest'));
+
+expectType<WithCopyEvents<{}>>(copy('source', 'dest', () => {}));
+
+type WithCopyEvents<T> = T & {
+	on(event: CopyEventType.ERROR, callback: (error: Error, info: CopyErrorInfo) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COMPLETE, callback: (info: Array<CopyOperation>) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_DIRECTORY_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.CREATE_SYMLINK_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_START, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_ERROR, callback: (error: Error, info: CopyOperation) => void): WithCopyEvents<T>;
+	on(event: CopyEventType.COPY_FILE_COMPLETE, callback: (info: CopyOperation) => void): WithCopyEvents<T>;
+}
+
 expectError(copy(123, 'dest'));
 expectError(copy('source', 123));
 expectError(copy('source', 'dest', 'options'));
